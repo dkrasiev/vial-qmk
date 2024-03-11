2139898b70ddd52bc72644f518158092bfc5ee79
Dmitry Krasiev <dkrasiev@datafire.ru>
2024-03-11 12:56:30 +0400

    feat: change LANG, SEARCH, QK_RBT and QK_BOOT location

--- keyboards/ergohaven/k02/keymaps/dkrasiev/keymap.c
+++ keyboards/ergohaven/k02/keymaps/dkrasiev/keymap.c
@@ -27,17 +27,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
         KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                            KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
         _______, _______, KC_HOME, KC_UP  , KC_END , KC_INS ,                            KC_PGUP, KC_7   , KC_8   , KC_9   , _______, _______,
-        SEARCH , _______, KC_LEFT, KC_DOWN, KC_RIGHT,KC_ENT ,                            KC_PGDN, KC_4   , KC_5   , KC_6   , _______, SEARCH ,
-        LANG   , _______, PREVWRD, KC_CAPS, NEXTWRD, KC_DEL ,                            KC_PSCR, KC_1   , KC_2   , KC_3   , _______, LANG   ,
-                          _______, _______, _______, _______, QK_BOOT,          _______, ADJUST , _______, KC_0   , _______
+        _______, LANG , KC_LEFT, KC_DOWN, KC_RIGHT,KC_ENT ,                            KC_PGDN, KC_4   , KC_5   , KC_6   , LANG   , _______,
+        _______, SEARCH , PREVWRD, KC_CAPS, NEXTWRD, KC_DEL ,                            KC_PSCR, KC_1   , KC_2   , KC_3   , SEARCH , _______,
+                          QK_BOOT, _______, _______, _______, _______,          _______, ADJUST , _______, KC_0   , _______
     ),
 
     [_RAISE] = LAYOUT(
         _______, _______, KC_AT  , KC_DLR , KC_HASH, _______,                            _______, _______, KC_CIRC, _______, _______, _______,
         _______, _______, KC_LT  , KC_EQL , KC_GT  , KC_GRV ,                            _______, KC_LBRC, KC_UNDS, KC_RBRC, _______, _______,
-        SEARCH , KC_BSLS, KC_LPRN, KC_MINS, KC_RPRN, KC_PLUS,                            KC_PERC, KC_LCBR, KC_SCLN, KC_RCBR, KC_EXLM, SEARCH ,
-        LANG   , _______, KC_ASTR, KC_COLN, KC_SLSH, _______,                            _______, KC_PIPE, KC_TILD, KC_AMPR, _______, LANG   ,
-                          _______, _______, _______, ADJUST , _______,          QK_BOOT, _______, _______, _______, QK_RBT
+        _______, KC_BSLS, KC_LPRN, KC_MINS, KC_RPRN, KC_PLUS,                            KC_PERC, KC_LCBR, KC_SCLN, KC_RCBR, KC_EXLM, _______,
+        _______, _______, KC_ASTR, KC_COLN, KC_SLSH, _______,                            _______, KC_PIPE, KC_TILD, KC_AMPR, _______, _______,
+                          _______, _______, _______, ADJUST , _______,          _______, _______, _______, QK_RBT , QK_BOOT
     ),
 
     [_ADJUST] = LAYOUT(
