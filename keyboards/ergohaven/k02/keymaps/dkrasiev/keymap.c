f3aee5f54f1fd4934800db96d9f86ebd4b02f3a5
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-03-13 22:31:48 +0400

    feat: remove default layout modifier keys

--- keyboards/ergohaven/k02/keymaps/dkrasiev/keymap.c
+++ keyboards/ergohaven/k02/keymaps/dkrasiev/keymap.c
@@ -12,8 +12,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                            KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
         KC_ESC , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                            KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
         KC_TAB , MT(MOD_LGUI, KC_A), MT(MOD_LALT, KC_S), MT(MOD_LSFT, KC_D), MT(MOD_LCTL, KC_F), KC_G, KC_H , MT(MOD_RCTL, KC_J), MT(MOD_RSFT, KC_K), MT(MOD_RALT, KC_L), MT(MOD_RGUI, KC_SCLN), KC_QUOT,
-        KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                            KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                          KC_LGUI, KC_LALT, KC_LCTL, LOWER  , KC_SPC ,          KC_ENT , RAISE  , KC_RCTL, KC_RALT, KC_RGUI
+        _______, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                            KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______,
+                          _______, _______, _______, LOWER  , KC_SPC ,          KC_ENT , RAISE  , _______, _______, _______
     ),
 
     [_GAMES] = LAYOUT(
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_LOWER] = LAYOUT(
         KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                            KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
-        _______, _______, KC_HOME, KC_UP  , KC_END , KC_INS ,                            KC_PGUP, KC_7   , KC_8   , KC_9   , _______, _______,
-        _______, LANG   , KC_LEFT, KC_DOWN, KC_RIGHT,KC_ENT ,                            KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT,LANG   , _______,
+        _______, _______, KC_HOME, _______, KC_END , KC_INS ,                            KC_PGUP, KC_7   , KC_8   , KC_9   , _______, _______,
+        _______, LANG   , _______, _______, _______,KC_ENT ,                            KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT,LANG   , _______,
         _______, SEARCH , PREVWRD, KC_CAPS, NEXTWRD, KC_DEL ,                            KC_PSCR, KC_1   , KC_2   , KC_3   , SEARCH , _______,
                           QK_BOOT, _______, _______, _______, _______,          _______, ADJUST , _______, KC_0   , _______
     ),
