5bcf541c393f173ee3c62c45675b05040a5b01fd
Dmitry Krasiev <dkrasiev@datafire.ru>
2024-03-13 15:26:31 +0400

    feat: sgac home row mod

--- keyboards/ergohaven/k02/keymaps/dkrasiev/keymap.c
+++ keyboards/ergohaven/k02/keymaps/dkrasiev/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                            KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_BSPC,
         KC_ESC , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                            KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS,
-        KC_TAB , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                            KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+        KC_TAB , MT(MOD_LSFT, KC_A), MT(MOD_LGUI, KC_S), MT(MOD_LALT, KC_D), MT(MOD_LCTL, KC_F), KC_G,                           KC_H   , MT(MOD_RCTL, KC_J), MT(MOD_RALT, KC_K), MT(MOD_RGUI, KC_L), MT(MOD_RSFT, KC_SCLN), KC_QUOT,
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                            KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
                           KC_LGUI, KC_LALT, KC_LCTL, LOWER  , KC_SPC ,          KC_ENT , RAISE  , KC_RCTL, KC_RALT, KC_RGUI
     ),
