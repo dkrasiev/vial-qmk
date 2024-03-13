6bd41f437e2ce0d0d9017b71bf3f0d82def81fb0
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-03-13 23:48:12 +0400

    style: format files

--- keyboards/ergohaven/k02/keymaps/dkrasiev/keymap.c
+++ keyboards/ergohaven/k02/keymaps/dkrasiev/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT(
         KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,                            KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
         _______, _______, KC_HOME, _______, KC_END , KC_INS ,                            KC_PGUP, KC_7   , KC_8   , KC_9   , _______, _______,
-        _______, LANG   , _______, _______, _______,KC_ENT ,                            KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT,LANG   , _______,
+        _______, LANG   , _______, _______, _______, KC_ENT ,                            KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT,LANG   , _______,
         _______, SEARCH , PREVWRD, KC_CAPS, NEXTWRD, KC_DEL ,                            KC_PSCR, KC_1   , KC_2   , KC_3   , SEARCH , _______,
                           QK_BOOT, _______, _______, _______, _______,          _______, ADJUST , _______, KC_0   , _______
     ),
