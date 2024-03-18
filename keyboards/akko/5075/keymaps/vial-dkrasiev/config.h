3364a241bc8d2c6c54658b1c03bbddce5e4bda12
Dmitry Krasiev <dmitrykrasiev@gmail.com>
2024-03-18 22:44:16 +0400

    feat(akko/5075): add home row mods

--- keyboards/akko/5075/keymaps/vial-dkrasiev/config.h
+++ keyboards/akko/5075/keymaps/vial-dkrasiev/config.h
@@ -5,3 +5,15 @@
 #define VIAL_KEYBOARD_UID {0x83, 0x85, 0x35, 0x7A, 0xB4, 0xD8, 0x47, 0x4E}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+// Left-hand home row mods
+#define GUI_A LGUI_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define SFT_D LSFT_T(KC_D)
+#define CTL_F LCTL_T(KC_F)
+
+// Right-hand home row mods
+#define CTL_J RCTL_T(KC_J)
+#define SFT_K RSFT_T(KC_K)
+#define ALT_L LALT_T(KC_L)
+#define GUI_SCLN RGUI_T(KC_SCLN)
